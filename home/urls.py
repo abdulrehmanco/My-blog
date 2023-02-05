a2419a4738032b9b12570b14d68febc6d2c33164
Abdul Rehman Rais <abdulrehmanrais123@gmail.com>
2023-02-05 11:09:48 +0500

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -17,5 +17,4 @@ urlpatterns = [
     path('postcomment',views.postcomment,name='postcomment'),
     path('<slug:slug>', views.detail, name='detail'),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
